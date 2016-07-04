9b3a7ff9952f7cf52b08e2be6a67d7c3072ae5fe
bellking <bellking@sk.com>
2016-07-04 16:51:49 +0900

    add slice by row

--- r-tutor.R
+++ r-tutor.R
@@ -1,8 +1,12 @@
-# data.frame reference
+# data.frame reference - column
 head(mtcars[[2]])
 class(mtcars[[2]])
 head(mtcars[,2])
 class(mtcars[,2])
-# data.frame slice
+# data.frame slice - column
 head(mtcars[2])
 class(mtcars[2])
+# data.frame slice -row
+L = mtcars$am == 0
+mtcars[L,]
+mtcars[L,]$mpg
